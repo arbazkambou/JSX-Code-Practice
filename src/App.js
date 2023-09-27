286430cde7cdbb0b6f11afb3d71022d17724ccbf
Arbaz Shoukat <arbazkamboh342@gmail.com>
2023-09-27 16:25:31 +0500

    JSX code practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+const number=7;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="app">
+    <h1>My Favorite Foods</h1>
+    <ul>
+     <li>Bacon</li>
+     <li>Jamon</li>
+     <li>Noodles</li>
+    </ul>
+    <p>Your lucky number {number}</p>
+   </div>
   );
 }
 
